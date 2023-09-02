bd67b1e8ae0e921a9a8e69607a9f479f6c5d14cb
Zorex <zorexsalvo@gmail.com>
2023-09-02 14:27:15 +0800

    Add drums

--- meteor_garden/rhythm_melody_and_drumbeats.py
+++ meteor_garden/rhythm_melody_and_drumbeats.py
@@ -1,6 +1,16 @@
 from musicpy import C, play, piece, chord, drum
 
-verse = (
+
+# K = Kick
+# H = Hi-hat
+# S = Snare
+# OH = Open Hi-hat
+# PH = Pedal Hi-hat
+# t = Triplet
+# r = Rest
+# drum('K, H, H, K, S, H, H, H, t:1').play()
+
+chorus = (
     C("C", 3, 1 / 4, 1 / 8) * 2
     | C("C", 3, 1 / 4)
     | C("G", 2, 1 / 4, 1 / 8) * 2
@@ -17,13 +27,16 @@ verse = (
     | C("F", 2, 1 / 4)
     | C("G", 2, 1 / 4, 1 / 8) * 2
     | C("G", 2, 1 / 4)
-)
+) * 2
+
+drum1 = drum("K, H, S, H, r:2, K, H, S, H, r:2").notes * 4
+drum1.set_volume(112)
 
 result = piece(
-    [verse] ,
-    [2, 34, 49, 31, 1],
+    [chorus, drum1] ,
+    [25, 1],
     bpm=105,
     start_times=[0, 0],
-    channels=[0, 1],
+    channels=[0, 9],
 )
 play(result)
