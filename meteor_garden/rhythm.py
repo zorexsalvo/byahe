86e8a51e98f383b10bd229e12ff447e73e1a4fa8
Zorex <zorexsalvo@gmail.com>
2023-09-02 13:43:27 +0800

    Add documentation

--- meteor_garden/rhythm.py
+++ meteor_garden/rhythm.py
@@ -1,22 +1,32 @@
 from musicpy import C, play
 
-guitar = (
-    C("C", 3, 1 / 4, 1 / 8) * 2
-    | C("C", 3, 1 / 4)
-    | C("G", 2, 1 / 4, 1 / 8) * 2
-    | C("G", 2, 1 / 4)
-    | C("Am", 2, 1 / 4, 1 / 8) * 2
-    | C("Am", 2, 1 / 4)
-    | C("Em", 2, 1 / 4, 1 / 8) * 2
-    | C("Em", 2, 1 / 4)
-    | C("F", 2, 1 / 4, 1 / 8) * 2
-    | C("F", 2, 1 / 4)
-    | C("Em", 2, 1 / 4, 1 / 8) * 2
-    | C("Em", 2, 1 / 4)
-    | C("F", 2, 1 / 4, 1 / 8) * 2
-    | C("F", 2, 1 / 4)
-    | C("G", 2, 1 / 4, 1 / 8) * 2
-    | C("G", 2, 1 / 4)
-) * 1
+def play_guitar_rhythm():
+    """
+    Plays a guitar rhythm using the musicpy library.
 
-play(guitar, bpm=90, instrument=25)
+    The rhythm consists of the following chords:
+    C, C, G, G, Am, Am, Em, Em, F, F, Em, Em, F, F, G, G
+
+    The function uses the `play` function from the musicpy library to play the rhythm.
+    The `bpm` parameter is set to 90 and the `instrument` parameter is set to 25.
+    """
+    guitar = (
+        C("C", 3, 1 / 4, 1 / 8) * 2
+        | C("C", 3, 1 / 4)
+        | C("G", 2, 1 / 4, 1 / 8) * 2
+        | C("G", 2, 1 / 4)
+        | C("Am", 2, 1 / 4, 1 / 8) * 2
+        | C("Am", 2, 1 / 4)
+        | C("Em", 2, 1 / 4, 1 / 8) * 2
+        | C("Em", 2, 1 / 4)
+        | C("F", 2, 1 / 4, 1 / 8) * 2
+        | C("F", 2, 1 / 4)
+        | C("Em", 2, 1 / 4, 1 / 8) * 2
+        | C("Em", 2, 1 / 4)
+        | C("F", 2, 1 / 4, 1 / 8) * 2
+        | C("F", 2, 1 / 4)
+        | C("G", 2, 1 / 4, 1 / 8) * 2
+        | C("G", 2, 1 / 4)
+    ) * 1
+
+    play(guitar, bpm=90, instrument=25)
